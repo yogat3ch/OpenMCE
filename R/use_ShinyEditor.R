f5d228d1d3459c3b237619ce47f593d06c92ddaa
Stephen Holsenbeck <stephen@virgalabs.io>
2022-11-30 07:09:52 -0500

    Add tinymce node_module

--- R/use_ShinyEditor.R
+++ R/use_ShinyEditor.R
@@ -5,20 +5,14 @@
 #' @return JS Files
 #' @export
 
-use_editor <- function(API = Sys.getenv("TINYMCE_KEY")) {
+use_editor <- function(API = Sys.getenv("TINYMCE_KEY"), path = system.file("node_modules/tinymce/tinymce.min.js", package = "ShinyEditor")) {
   if (UU::zchar(API)) {
     UU::gwarn("No API Key provided. See {.code tinymce_key} to set one up.")
   }
 
-  tagList(shiny::singleton(
-    shiny::tags$head(
+  shiny::tagList(shiny::singleton(
+    htmltools::attachDependencies(shiny::tags$head(
       shinyjs::useShinyjs(),
-      shiny::tags$script(
-        src = paste0(
-          "https://cdn.tiny.cloud/1/", API, "/tinymce/5/tinymce.min.js"
-        ),
-        referrerpolicy = "origin"
-      ),
       # shiny::tags$script(src = "ShinyEditor-assets/ShinyEditor.js"),
       shiny::tags$script(
       "Shiny.addCustomMessageHandler('HTMLText', function(data) {
@@ -29,7 +23,26 @@ use_editor <- function(API = Sys.getenv("TINYMCE_KEY")) {
         "Shiny.addCustomMessageHandler('UpdateshinyEditor', function(data) {
         tinyMCE.get(data.id).setContent(data.content);
         $('#'+data.id).trigger('change');});")
-    )
+    ), tiny_mce_dependency())
   ))
 
+}
+
+#' Include the tinyMCE javascript dependencies
+#'
+#' @return \code{html_dependency}
+#' @export
+#'
+
+tiny_mce_dependency <- function() {
+  shiny::addResourcePath("tinymce-6.3.0", system.file("node_modules/tinymce", package = "ShinyEditor"))
+  htmltools::htmlDependency(
+    name = "tinyMCE",
+    version = "6.3.0",
+    package = "ShinyEditor",
+    src = c(
+      file = "node_modules"
+    ),
+    script = "tinymce/tinymce.min.js"
+  )
 }
